eac686f319e25b530ac42106daa0da6adfa71152
Andrew Lebedev <jediandrewkin@gmail.com>
2020-09-20 14:27:02 +0300

    First commit

---
+++ tableview-app/Controller/CategoriesVC.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  tableview-app
+//
+//  Created by Андрей Лебедев on 20.09.2020.
+//
+
+import UIKit
+
+class CategoriesVC: UIViewController {
+    
+    @IBOutlet weak var categoryTable: UITableView!
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ tableview-app/View/CategoryCell.swift
@@ -0,0 +1,26 @@
+//
+//  CategoryCell.swift
+//  tableview-app
+//
+//  Created by Андрей Лебедев on 20.09.2020.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+    
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryTitle: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
